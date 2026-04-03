3f3de9c4a84169e6b2d3b76aea229742dd7824e7
Vanessa De Jager <vanessa.dejager+gonz@gobss.nz>
2026-04-03 22:03:32 +0200

    Add category management components and services; implement CRUD operations for categories

---
+++ src/main/java/org/ecommerce/common/exception/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package org.ecommerce.common.exception;
+
+public class CategoryNotFoundException extends RuntimeException
+{
+    public CategoryNotFoundException(String message)
+    {
+        super(message);
+    }
+}
--- src/main/java/org/ecommerce/common/repository/CategoryRepository.java
+++ src/main/java/org/ecommerce/common/repository/CategoryRepository.java
@@ -1,10 +1,34 @@
 package org.ecommerce.common.repository;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.ecommerce.common.entity.CategoryEntity;
 
+import java.util.List;
+import java.util.UUID;
+
 @ApplicationScoped
-public class CategoryRepository implements PanacheRepository<CategoryEntity>
+public class CategoryRepository extends BaseRepository<CategoryEntity, UUID>
 {
+    // returns all the sub-categories
+    public List<CategoryEntity> findSubCategoriesByParentId(Long parentId)
+    {
+        return list("parent.id", parentId);
+    }
+
+    public CategoryEntity findByNameExcludingId(String name, UUID excludeId)
+    {
+        if (excludeId == null) {
+            return find("lower(name) = lower(?1)", name).firstResult();
+        }
+        return find("lower(name) = lower(?1) and id != ?2", name, excludeId).firstResult();
+    }
+
+    public CategoryEntity findBySlugExcludingId(String slug, Object excludeId)
+    {
+        if (excludeId == null) {
+            return find("lower(slug) = lower(?1)", slug).firstResult();
+        }
+
+        return find("lower(slug) = lower(?1) and id != ?2", slug, excludeId).firstResult();
+    }
 }
