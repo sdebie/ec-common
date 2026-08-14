92120b4f57c28c044dd74a62f45d784e46594b2f
Vanessa De Jager <vanessa.dejager+gonz@gobss.nz>
2026-08-14 23:27:12 +0200

    Enhance order contact management by restricting updates to orders in CREATED status and updating related tests

--- src/test/java/org/ecommerce/common/query/FieldNameValidatorTest.java
+++ src/test/java/org/ecommerce/common/query/FieldNameValidatorTest.java
@@ -2,7 +2,6 @@ package org.ecommerce.common.query;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
