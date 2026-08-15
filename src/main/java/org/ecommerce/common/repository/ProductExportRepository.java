9d842563d85588bbeadf2db2f165edc80e6365ac
Vanessa De Jager <vanessa.dejager+gonz@gobss.nz>
2026-08-15 10:50:36 +0200

    Refactor testimonial handling by consolidating request DTOs and removing unused repositories

--- src/main/java/org/ecommerce/common/repository/ProductExportRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ecommerce.common.repository;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import org.ecommerce.common.entity.ProductImageEntity;
-
-public class ProductExportRepository implements PanacheRepository<ProductImageEntity> {
-}
